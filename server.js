8b6c7d8414fd3abbe515c6d7083561e81fb4e715
James Ng <james_ng@ig.com.br>
2021-04-14 18:47:34 -0300

    trabalhando com postman

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonserver = require('json-server');
+
+const server = jsonserver.create();
+
+const router = jsonserver.router('db.json');
+
+const middlewares = jsonserver.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, ()=> {
+    console.log(`JSON Server está rodando na porta ${port}`);
+});
